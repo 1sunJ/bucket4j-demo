3a9144047b2c6aed2e8ccdb2139cd1aa34ef5861
1sunJ <won9619a@gmail.com>
2025-04-29 15:16:30 +0900

    initializing project

---
+++ src/test/java/com/example/bucket4j_demo/Bucket4jDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bucket4j_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bucket4jDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
